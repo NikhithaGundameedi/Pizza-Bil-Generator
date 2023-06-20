c7bbcd5c38bfd93445070cd0e6b20d7b0d2a6fd0
Nikhitha <116160147+NikhithaGundameedi@users.noreply.github.com>
2023-06-20 08:49:07 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,114 @@
+    var TotalPrice=0;
+    var price=0;
+    document.querySelector('.btn1').onclick=function(){
+        price=document.querySelector('.price-tag1').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+    } 
+    document.querySelector('.btn2').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag2').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn3').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag3').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn4').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag4').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn5').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag5').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn6').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag6').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn7').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag7').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn8').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag8').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn9').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag9').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn10').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag10').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn11').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag11').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn12').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag12').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn13').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag13').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn14').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag14').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn15').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag15').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+    document.querySelector('.btn16').onclick=function(){
+        price=0;
+        price=document.querySelector('.price-tag16').textContent;
+        price=parseInt(price.replace(/[^0-9]/g, ""));
+        TotalPrice=TotalPrice+price;
+        console.log(TotalPrice);
+    } 
+  
+  
---
+++ login.js
@@ -0,0 +1,6 @@
+document.querySelector('button').onclick=function(){
+   let name= document.querySelector('.name').value;
+   let mbl= document.querySelector('.mbl').value;
+   let mail= document.querySelector('.mail').value;
+   let loc= document.querySelector('.loc').value;
+}
